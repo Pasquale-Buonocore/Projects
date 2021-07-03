d60e07a64249d195101b848429674ed383130568
Pasquale-Buonocore <pasquale.buonocore@hotmail.it>
2021-07-03 17:21:28 +0200

    Update Python_DataStructyres.py
    Modifica struttura script Data structure

--- Python/Python_DataStructyres.py
+++ Python/Python_DataStructyres.py
@@ -1,14 +1,39 @@
-# List
-Prova_Lista = ["ciao","Lista"]
-print(Prova_Lista)
+import sys
 
-Prova_Lista.append(str(5))
-print(Prova_Lista)
+# Function to test lists and their methods
 
-#Dictionary
-Prova_Dict = {"name" : "John", "age" : 36}
-print(type(Prova_Dict))
 
-value = '130.900'
+def TestLists():
+    print("Testing LISTS")
+    # List
+    Prova_Lista = ["ciao", "Lista"]
+    print(Prova_Lista)
 
-print(value)
+    Prova_Lista.append(str(5))
+    print(Prova_Lista)
+
+# Function to test dictionaries and their methods
+
+
+def TestDictionary():
+    print("Testing DICTIONARIES")
+    # Dictionary
+    Prova_Dict = {"name": "John", "age": 36}
+    print(type(Prova_Dict))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        if sys.argv[1] in ["list", "dict", "tuple", "set"]:
+            if sys.argv[1] == "list":
+                TestLists()
+            elif sys.argv[1] == "dict":
+                TestDictionary()
+            else:
+                print("Tuple and set needs to be implemented")
+        else:
+            print(
+                "Wrong argument passed. Possible arguments: [list, dict, tuple, set]")
+    else:
+        print("No arguments passed. What do you want to test?")
+        print("Possible arguments: [list, dict, tuple, set]")
