7d27211d210e45e804f96655a1fd7f5c2c9087c5
Pasquale-Buonocore <pasquale.buonocore@hotmail.it>
2021-07-03 16:25:29 +0200

    Commit nuova struttura
    Commit nuova struttura

---
+++ Python/Python_DataStructyres.py
@@ -0,0 +1,14 @@
+# List
+Prova_Lista = ["ciao","Lista"]
+print(Prova_Lista)
+
+Prova_Lista.append(str(5))
+print(Prova_Lista)
+
+#Dictionary
+Prova_Dict = {"name" : "John", "age" : 36}
+print(type(Prova_Dict))
+
+value = '130.900'
+
+print(value)
